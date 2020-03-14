ffb28329f8cfb99ba4bb81ebc246eb14bfec5778
wenfengcheng <wenfeng.funkyxamarin@gmail.com>
2020-03-14 16:28:10 +0800

    add origin swift code

---
+++ Source/ScrollingNavigationViewController.swift
@@ -0,0 +1,43 @@
+import UIKit
+
+/**
+ A custom `UIViewController` that implements the base configuration.
+ */
+@objc(ScrollingNavigationViewController)
+open class ScrollingNavigationViewController: UIViewController, UIScrollViewDelegate, UINavigationControllerDelegate {
+
+  // MARK: - ScrollView config
+
+  /**
+   On appear calls `showNavbar()` by default
+   */
+  override open func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+
+    if let navigationController = self.navigationController as? ScrollingNavigationController {
+      navigationController.showNavbar(animated: true)
+    }
+  }
+
+  /**
+   On disappear calls `stopFollowingScrollView()` to stop observing the current scroll view, and perform the tear down
+   */
+  override open func viewWillDisappear(_ animated: Bool) {
+    super.viewWillDisappear(animated)
+
+    if let navigationController = self.navigationController as? ScrollingNavigationController {
+      navigationController.stopFollowingScrollView()
+    }
+  }
+
+  /**
+   Calls `showNavbar()` with the `scrollToTop` parameter to `true` when a scroll to top is requested
+   */
+  open func scrollViewShouldScrollToTop(_ scrollView: UIScrollView) -> Bool {
+    if let navigationController = self.navigationController as? ScrollingNavigationController {
+      navigationController.showNavbar(animated: true, scrollToTop: true)
+    }
+    return true
+  }
+
+}
